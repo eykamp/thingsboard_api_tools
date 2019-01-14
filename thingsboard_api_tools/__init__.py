5e8997ebf312482dc2b4bc9835e16ee6d54760b6
Unknown <chris@eykamp.com>
2019-01-13 23:39:08 -0800

    Normalize formatting, comments, and whitespace

--- thingsboard_api_tools/__init__.py
+++ thingsboard_api_tools/__init__.py
@@ -17,7 +17,9 @@
 
 # Update with: pip install git+git://github.com/eykamp/thingsboard_api_tools.git --upgrade
 
-import json, requests, time
+import json
+import requests
+import time
 from http import HTTPStatus
 
 
@@ -35,9 +37,10 @@ class TbApi:
         self.verbose = False
 
 
-    ''' Fetches and return an access token needed by most other methods; caches tokens for reuse '''
     def get_token(self):
-
+        """
+        Fetches and return an access token needed by most other methods; caches tokens for reuse
+        """
         # If we already have a valid token, use it
         if self.token is not None and time.time() - self.token_time < self.token_timeout:
             return self.token
@@ -56,13 +59,17 @@ class TbApi:
         return self.token
 
 
-    ''' Return a list of all customers in the system '''
     def get_users(self):
+        """
+        Return a list of all customers in the system
+        """
         return self.get('/api/customers?limit=99999', "Error retrieving customers")["data"]
 
 
-    ''' Get customer with specified name '''
     def get_customer(self, name):
+        """
+        Get customer with specified name
+        """
         customers = self.get('/api/customers?limit=99999&textSearch=' + name, "Can't find customer with name '" + name + "'")
         for customer in customers['data']:
             if(customer['title'] == name):
@@ -71,37 +78,51 @@ class TbApi:
         return None
 
 
-    ''' Returns a list of all devices for current tenant `'''
     def get_tenant_devices(self):
+        """
+        Returns a list of all devices for current tenant
+        """
         return self.get('/api/tenant/devices?limit=99999', "Error retrieving devices for tenant")["data"]
-        
 
-    ''' Returns a list of all devices associated with a customer; pass in customer object or id '''
+
     def get_customer_devices(self, cust):
+        """
+        Returns a list of all devices associated with a customer; pass in customer object or id
+        """
         cust_id = self.get_id(cust)
 
         return self.get('/api/customer/' + cust_id + '/devices?limit=99999', "Error retrieving devices for customer '" + cust_id + "'")["data"]
 
 
-    ''' Returns UUID of public customer, or None if there is none '''
     def get_public_user_uuid(self):
+        """
+        Returns UUID of public customer, or None if there is none
+        """
         return self.get_user_uuid('Public')
 
 
-    ''' Returns UUID of named customer, or None if user not found '''
     def get_user_uuid(self, name):
+        """
+        Returns UUID of named customer, or None if user not found
+        """
         return self.get_id(self.get_customer(name))
+
 
     def get_customer_by_id(self, cust_id):
         return self.get('/api/customer/' + cust_id, "Could not retrieve customer with id '" + cust_id + "'")
 
+
     def get_customers_by_name(self, cust_name_prefix):
-        ''' Returns a list of all customers starting with the specified name '''
+        """
+        Returns a list of all customers starting with the specified name
+        """
         return self.get('/api/customers?limit=99999&textSearch=' + cust_name_prefix, "Error retrieving customers with names starting with '" + cust_name_prefix + "'")['data']
 
 
     def get_customer_by_name(self, cust_name):
-        ''' Returns a customer with the specified name, or None if we can't find one '''
+        """
+        Returns a customer with the specified name, or None if we can't find one
+        """
         custs = self.get_customers_by_name(cust_name)
         for cust in custs:
             if cust['title'] == cust_name:
@@ -109,10 +130,8 @@ class TbApi:
 
         return None
 
-
     def update_customer(self, cust, name=None, address=None, address2=None, city=None, state=None, zip=None, country=None, email=None, phone=None, additional_info=None):
-        ''' Updates an existing customer record --> pass in customer object, or a customer id
-            additional_info is a dict '''
+        """ Updates an existing customer record --> pass in customer object, or a customer id additional_info is a dict """
 
         # Check if user passed a customer_id; if so, retrieve the customer object
         if isinstance(cust, str):
@@ -129,7 +148,7 @@ class TbApi:
         if state is not None:
             cust["state"] = state
         if zip is not None:
-            cust["zip"]= zip
+            cust["zip"] = zip
         if country is not None:
             cust["country"] = country
         if email is not None:
@@ -143,7 +162,7 @@ class TbApi:
 
 
     def add_customer(self, name, address, address2, city, state, zip, country, email, phone, additional_info=None):
-        ''' Adds customer and returns JSON customer from database '''
+        """ Adds customer and returns JSON customer from database """
         data = {
             "title": name,
             "address": address,
@@ -162,13 +181,17 @@ class TbApi:
         return self.post('/api/customer', data, "Error adding customer '" + name + "'")
 
 
-    ''' Returns True if successful, False if the customer wasn't found '''
     def delete_customer_by_id(self, id):
+        """
+        Returns True if successful, False if the customer wasn't found
+        """
         return self.delete("/api/customer/" + id, "Error deleting customer '" + id + "'")
 
 
-    ''' Returns True if successful, False if the customer wasn't found '''
     def delete_customer_by_name(self, name):
+        """
+        Returns True if successful, False if the customer wasn't found
+        """
         id = self.get_user_uuid(name)
         if id == None:
             print("Could not find customer with name " + name)
@@ -177,21 +200,27 @@ class TbApi:
         return self.delete_customer_by_id(id)
 
 
-    ''' Returns dashboard definition '''
     def assign_dash_to_user(self, dash, customer):
+        """
+        Returns dashboard definition
+        """
         dashboard_id = self.get_id(dash)
         customer_id = self.get_id(customer)
         return self.post('/api/customer/' + customer_id + '/dashboard/' + dashboard_id, None, "Could not assign dashboard '" + dashboard_id + "' to customer '" + customer_id + "'")
 
 
-    ''' Returns True if dashboard was deleted, False if it did not exist '''
     def delete_dashboard(self, dash):
+        """
+        Returns True if dashboard was deleted, False if it did not exist
+        """
         dashboard_id = self.get_id(dash)
         return self.delete('/api/dashboard/' + dashboard_id, "Error deleting dashboard '" + dashboard_id + "'")
 
 
-    ''' Returns dashboard definition '''
     def create_dashboard_for_customer(self, dash_name, dash_def):
+        """
+        Returns dashboard definition
+        """
 
         data = {
             "configuration": dash_def["configuration"],
@@ -203,18 +232,24 @@ class TbApi:
         return self.post('/api/dashboard', data, "Error creating new dashboard")
 
 
-    ''' Saves a fully formed dashboard definition '''
     def save_dashboard(self, dash_def):
+        """
+        Saves a fully formed dashboard definition
+        """
         return self.post('/api/dashboard', dash_def, "Error saving dashboard")
 
 
-    ''' Returns a list of all dashes starting with the specified name '''
     def get_dashboards_by_name(self, dash_name_prefix):
+        """
+        Returns a list of all dashes starting with the specified name
+        """
         return self.get('/api/tenant/dashboards?limit=99999&textSearch=' + dash_name_prefix, "Error retrieving dashboards starting with '" + dash_name_prefix + "'")['data']
 
 
-    ''' Returns dashboard with specified name, or None if we can't find one '''
     def get_dashboard_by_name(self, dash_name):
+        """
+        Returns dashboard with specified name, or None if we can't find one
+        """
         dashes = self.get_dashboards_by_name(dash_name)
         for dash in dashes:
             if dash['title'] == dash_name:
@@ -223,8 +258,10 @@ class TbApi:
         return None
 
 
-    ''' Retrieve dashboard by id '''
     def get_dashboard_by_id(self, dash):
+        """
+        Retrieve dashboard by id
+        """
         dash_id = self.get_id(dash)
         return self.get('/api/dashboard/info/' + dash_id, "Error retrieving dashboard for '" + dash_id + "'")
 
@@ -233,8 +270,11 @@ class TbApi:
         dash_id = self.get_id(dash)
         return self.get('/api/dashboard/' + dash_id, "Error retrieving dashboard definition for '" + dash_id + "'")
 
-    ''' Returns named device object, or None if it can't be found '''
+
     def get_device_by_id(self, device_id):
+        """
+        Returns named device object, or None if it can't be found
+        """
         if device_id is None:
             return None
         try:
@@ -245,8 +285,11 @@ class TbApi:
             else:
                 raise ex
 
-    ''' Returns named device object, or None if it can't be found '''
+
     def get_device_by_name(self, device_name):
+        """
+        Returns named device object, or None if it can't be found
+        """
         devices = self.get_devices_by_name(device_name)
 
         # Fine exact match
@@ -257,8 +300,10 @@ class TbApi:
         return None
 
 
-    ''' Returns a list of all devices starting with the specified name '''
     def get_devices_by_name(self, device_name_prefix):
+        """
+        Returns a list of all devices starting with the specified name
+        """
         return self.get('/api/tenant/devices?limit=99999&textSearch=' + device_name_prefix, "Error fetching devices with name matching '" + device_name_prefix + "'")['data']
 
 
@@ -266,8 +311,10 @@ class TbApi:
         return self.get("/api/tenant/devices?limit=99999", "Error fetching list of all devices")['data']
 
 
-    ''' Returns device object '''
     def add_device(self, device_name, device_type, shared_attributes, server_attributes):
+        """
+        Returns device object
+        """
 
         data = {
             "name": device_name,
@@ -282,52 +329,68 @@ class TbApi:
 
         if shared_attributes is not None:
             self.set_shared_attributes(device_id, shared_attributes)
-        
+
         return device
 
 
-    ''' Pass in a device or a device_id '''
     def get_device_token(self, device):
+        """
+        Pass in a device or a device_id
+        """
         device_id = self.get_id(device)
-            
+
         json = self.get("/api/device/" + device_id + "/credentials", "Error retreiving device_key for device '" + device_id + "'")
         return json["credentialsId"]
 
 
-    ''' Pass in a device or a device_id '''
     def get_server_attributes(self, device):
+        """
+        Pass in a device or a device_id
+        """
         return self.get_attributes(device, 'SERVER_SCOPE')
 
 
-    ''' Pass in a device or a device_id '''
     def get_shared_attributes(self, device):
+        """
+        Pass in a device or a device_id
+        """
         return self.get_attributes(device, 'SHARED_SCOPE')
 
 
-    ''' Pass in a device or a device_id '''
     def get_client_attributes(self, device):
+        """
+        Pass in a device or a device_id
+        """
         return self.get_attributes(device, 'CLIENT_SCOPE')
 
 
-    ''' Pass in a device or a device_id '''
     def get_attributes(self, device, scope):
+        """
+        Pass in a device or a device_id
+        """
         device_id = self.get_id(device)
 
         return self.get("/api/plugins/telemetry/DEVICE/" + device_id + "/values/attributes/" + scope, "Error retrieving " + scope + " attributes for '" + device_id + "'")
 
 
-    ''' Pass in a device or a device_id '''
     def set_server_attributes(self, device, attributes):
+        """
+        Pass in a device or a device_id
+        """
         return self.set_attributes(device, attributes, 'SERVER_SCOPE')
 
 
-    ''' Pass in a device or a device_id '''
     def set_shared_attributes(self, device, attributes):
+        """
+        Pass in a device or a device_id
+        """
         return self.set_attributes(device, attributes, 'SHARED_SCOPE')
 
 
-    ''' Pass in a device or a device_id '''
     def set_client_attributes(self, device, attributes):
+        """
+        Pass in a device or a device_id
+        """
         return self.set_attributes(device, attributes, 'CLIENT_SCOPE')
 
 
@@ -337,13 +400,12 @@ class TbApi:
         return self.post('/api/plugins/telemetry/DEVICE/' + device_id + '/' + scope, attributes, "Error setting " + scope + " attributes for device '" + device + "'")
 
 
-
     def send_telemetry(self, device_token, data, timestamp=None):
         """
         Note that this requires the device's secret token, not the device_id!
         """
         if timestamp is not None:
-            data = {"ts": timestamp ,"values": data }
+            data = {"ts": timestamp, "values": data}
         return self.post("/api/v1/" + device_token + "/telemetry", data, "Error sending telemetry for device with token '" + device_token + "'")
 
 
@@ -387,13 +449,14 @@ class TbApi:
         if interval is None:
             interval = 60 * 1000   # 1 minute, in ms
 
-        if limit is None:            
+        if limit is None:
             limit = 100
 
         if agg is None:
             agg = "NONE"   # MIN, MAX, AVG, SUM, COUNT, NONE
 
-        params = "/api/plugins/telemetry/DEVICE/" + device_id + "/values/timeseries?keys=" + keys + "&startTs=" + str(int(startTime)) + "&endTs=" + str(int(endTime)) + "&interval=" + str(interval) + "&limit=" + str(limit) + "&agg=" + agg
+        params = "/api/plugins/telemetry/DEVICE/" + device_id + "/values/timeseries?keys=" + keys + "&startTs=" + \
+            str(int(startTime)) + "&endTs=" + str(int(endTime)) + "&interval=" + str(interval) + "&limit=" + str(limit) + "&agg=" + agg
         error_message = "Error retrieving telemetry for device '" + device_id + "' with date range '" + str(int(startTime)) + "-" + str(int(endTime)) + "' and keys '" + keys + "'"
 
         return self.get(params, error_message)
@@ -449,9 +512,12 @@ class TbApi:
         return self.post('/api/customer/public/device/' + device_id, None, "Error assigning device '" + device_id + "' to public customer")
 
 
-    ''' Returns True if device was deleted, False if it did not exist '''
+
     def delete_device(self, device_id):
-        return self.delete('/api/device/' + device_id, "Error deleting device '" +  device_id + "'")
+        """
+        Returns True if device was deleted, False if it did not exist
+        """
+        return self.delete('/api/device/' + device_id, "Error deleting device '" + device_id + "'")
 
 
     @staticmethod
@@ -459,13 +525,14 @@ class TbApi:
         print('{}\n{}\n{}\n\n{}'.format('-----------START-----------', req.method + ' ' + req.url, '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()), req.body, ))
 
 
-    ''' Modifies headers '''
     def add_auth_header(self, headers):
+        """
+        Modifies headers
+        """
         token = self.get_token()
         if token is not None:
             headers['X-Authorization'] = 'Bearer ' + token
-            
-        
+
 
     def get(self, params, msg):
         url = self.mothership_url + params
@@ -518,7 +585,7 @@ class TbApi:
         self.validate_response(response, msg)
 
         if response.text is None or response.text == "":
-            return { }
+            return {}
 
         return json.loads(response.text)
 
